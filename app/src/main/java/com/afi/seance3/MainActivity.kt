964457c5a4b00ac1537db61be25c76207c2e59c6
Moustapha LY <moustaphaly1996@gmail.com>
2024-06-25 23:06:31 +0000

    fin seance

---
+++ app/src/main/java/com/afi/seance3/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.afi.seance3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.afi.seance3.ui.theme.Seance3Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContent {
+//            Seance3Theme {
+//                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colorScheme.background
+//                ) {
+//                    Greeting("Android")
+//                }
+//            }
+//        }
+//        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_connexion)
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name! Bonsoir a toutes",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Seance3Theme {
+        Greeting("Android")
+    }
+}
